3b82d0785e33a425bf559d096d7834f68c5dcd0c
Kameron R. Sween <kameron@krsween.com>
2016-11-05 15:58:38 -0500

    Added filters to jobs.

--- app/scripts/jobs/jobs.js
+++ app/scripts/jobs/jobs.js
@@ -10,4 +10,22 @@
 angular.module('anonymiaApp')
     .controller('JobsCtrl', function ($scope, jobsFactory) {
         $scope.jobs = jobsFactory.get();
+
+        $scope.companies = [];
+        $scope.experience = [];
+        $scope.dates = [];
+
+        $scope.jobs.forEach(function (job) {
+            if ($scope.companies.indexOf(job.company) === -1) {
+                $scope.companies.push(job.company);
+            }
+
+            if ($scope.experience.indexOf(job.experience) === -1) {
+                $scope.experience.push(job.experience);
+            }
+
+            if ($scope.dates.indexOf(job.dateAdded) === -1) {
+                $scope.dates.push(job.dateAdded);
+            }
+        });
     });
