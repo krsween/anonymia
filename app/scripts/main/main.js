7f382c6aed058c7c4a574be08eac9ec131842447
Kameron R. Sween <kameron@krsween.com>
2016-11-05 14:37:11 -0500

    tweaks

---
+++ app/scripts/main/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anonymiaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the anonymiaApp
+ */
+angular.module('anonymiaApp')
+    .controller('MainCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
